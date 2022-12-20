ff96b5c974e26f075f6e86f6d5386d3ef2d57715
hawyar <hawyarxoshnaw@gmail.com>
2022-12-19 23:41:44 -0500

    clean

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-## Node.js lib starter
+## athena-query
 
 ![test](https://github.com/hawyar/node-lib-starter/actions/workflows/test.yml/badge.svg)
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-### Clone
+### Usage
 
 ```bash
-git clone https://github.com/hawyar/node-lib-starter.git lib-name
+npm i athena-query
 ```
 
-### Test
+```javascript
+import { query } from 'athena-query'
+// const { query } = require('athena-query')
 
-```bash
-npm run test
-```
+await query('SELECT * FROM my_table')
 
-### Docs
-
-```bash
-npm run generate:docs
-// then run npm run serv
-```
-
-### Build
-
-```bash
-npm run build
+// with options:
+// await query('SELECT * FROM my_table', {
+//  database: 'default',
+//  region: 'us-east-1',
+//  profile: 'team-dev',
+//  workgroup: 'etl'
+//  bucket: 'my-super-bucket'
+//  backoff: 3000
+//  })
 ```
--- test.js
+++ test.js
@@ -1,6 +1,32 @@
 import tap from 'tap'
+import { query } from './lib/athena.js'
 
-tap.test('test', async t => {
-  t.pass('test')
+tap.test('query', async t => {
+  t.test('throw if no query is provided', async t => {
+    try {
+      await query()
+    } catch (error) {
+      t.equal(error.message, 'query string is required')
+    }
+  })
+
+  t.test('throw if no bucket is provided', async t => {
+    try {
+      await query('SELECT * FROM "sales" limit 10;')
+    } catch (error) {
+      t.equal(error.message, 'output bucket is required')
+    }
+  })
+  t.test('row and column', async t => {
+    const { columns, rows } = await query('SELECT * FROM "sales" where country = \'Tuvalu\';', {
+      database: 'default',
+      workGroup: 'primary',
+      profile: 'hawyar',
+      bucket: 'dundermifflinco-output'
+    })
+    t.equal(rows[0].country, 'Tuvalu')
+    t.equal(rows.length, 1)
+    t.equal(columns.length, 14)
+  })
   t.end()
 })
